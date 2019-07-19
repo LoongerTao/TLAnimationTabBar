5986de7bb38a67bba4ec7720566711f8298e5592
Gdxy <gxdy@email.com>
2019-07-19 18:07:02 +0800

    调整

---
+++ TLAnimationTabBar.podspec
@@ -0,0 +1,32 @@
+
+Pod::Spec.new do |s|
+  s.name         = 'TLAnimationTabBar'
+  s.version      = '1.0.0'
+  s.license      = 'MIT'
+  s.ios.deployment_target = '8.0'
+  s.platform     = :ios, '8.0'
+  s.summary      = 'Animation For TabBarItem'
+  s.homepage     = 'https://github.com/LoongerTao/TLAnimationTabBar'
+  s.author       = { 'LoongerTao' => '495285195@qq.com' }
+  s.requires_arc = true
+  s.source       = { :git => 'https://github.com/LoongerTao/TLAnimationTabBar.git', :tag => s.version }
+  s.public_header_files = 'TLAnimationTabBar/TLAnimationTabBar.h'
+  s.source_files = 'TLAnimationTabBar/TLAnimationTabBar.h'
+  
+  
+
+  s.subspec 'Config' do |cfg|
+      cfg.source_files = 'TLAnimationTabBar/*.{h,m}'
+      cfg.ios.frameworks = 'UIKit'
+  end
+
+end
+
+
+# 错误：xcodebuild: Returned an unsuccessful exit code. 
+# 一般是有头文件相互依赖，pod检测通不过
+#
+
+# [!] There was an error pushing a new version to trunk: execution expired
+# 网络问题，更换网络
+
--- TabBarDemo/TabBar/ViewController.m
+++ TabBarDemo/TabBar/ViewController.m
@@ -17,35 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(155, 200, 60, 60)];
-    [btn setImage:[UIImage imageNamed:@"Tools_00065"] forState:UIControlStateNormal];
-    btn.backgroundColor = [UIColor colorWithWhite:0.3 alpha:0.2];
-    btn.clipsToBounds = NO;
-    btn.layer.masksToBounds = NO;
-    [self.view addSubview:btn];
-    _btn = btn;
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    playSelectAnimation2(_btn);
     
-    if ([self.tabBarController.childViewControllers indexOfObject:self] == 0) {
-        self.tabBarItem.badgeValue = @(arc4random_uniform(10) + 1).stringValue;
-    }
-}
-
-void playSelectAnimation2(UIButton *btn) {
-
-    static NSInteger i = 1;
-    TLTransitionAniamtion *anmation = [TLTransitionAniamtion new];
-    anmation.direction = i++;
-    [anmation playSelectAnimationWhitTabBarButton:btn
-                                  buttonImageView:btn.imageView
-                                  buttonTextLabel:btn.titleLabel];
-//    NSLog(@"i = %zi", anmation.direction);
-    if (i == 8) {
-        i = 1;
-    }
 }
 
 @end
