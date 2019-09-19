c4eb646a0f41783820c5a73f9c4a69415732ee0d
Gdxy <gxdy@email.com>
2019-09-19 17:36:31 +0800

    ...

--- TLAnimationTabBar/TLAnimationTabBar.h
+++ TLAnimationTabBar/TLAnimationTabBar.h
@@ -9,8 +9,8 @@
 #ifndef Header_h
 #define Header_h
 
-#import "UITabBarItem+TLAnimation.h"
-#import "UITabBar+TLAnimation.h"
 #import "TLTabBarAnimation.h"
+#import "UITabBar+TLAnimation.h"
+#import "UITabBarItem+TLAnimation.h"
 
 #endif /* Header_h */
