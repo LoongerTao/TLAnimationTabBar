ed101cb758d7ce75d49eae828f3ab10c3674f78f
Gdxy <gxdy@email.com>
2020-01-13 17:35:56 +0800

    ...

---
+++ TabBarDemo/TabBar/iOS13 New Features/UIMenu/QMUICollectionViewPagingLayout.h
@@ -0,0 +1,17 @@
+//
+//  TLLineLayout.h
+//  TabBar
+//
+//  Created by 故乡的云 on 2019/10/18.
+//  Copyright © 2019 故乡的云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TLLineLayout : UICollectionViewFlowLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
